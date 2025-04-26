7054514e1af95d07ed428e3c016a77d43cea4977
kafeineholic <mind.pattranith@gmail.com>
2025-04-27 00:43:40 +0700

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Pattranith Ruangrotch on 27/4/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
