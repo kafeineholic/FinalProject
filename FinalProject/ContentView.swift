0ba0a87d3484220993a62b9af6fc9939da0b91d9
lbib <rhitakane@gmail.com>
2025-04-27 01:07:33 +0700

    Doraemon test push back

--- FinalProject/ContentView.swift
+++ FinalProject/ContentView.swift
@@ -1,15 +1,9 @@
-//
-//  ContentView.swift
-//  FinalProject
-//
-//  Created by Pattranith Ruangrotch on 27/4/2568 BE.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
        Text("test")
+        Text("DoraemonTest")
     }
 }
 
