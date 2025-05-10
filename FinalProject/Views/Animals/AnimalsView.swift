67393c0fd96a7504a96887676bc10634333d186b
lbib <rhitakane@gmail.com>
2025-05-10 19:23:13 +0700

    Update UserInterfaceState.xcuserstate

--- FinalProject/Views/Animals/AnimalsView.swift
+++ FinalProject/Views/Animals/AnimalsView.swift
@@ -4,5 +4,17 @@ struct AnimalsView: View {
     @StateObject var viewModel = AnimalsViewModel()
     
     var body: some View {
+        VStack {
+            if viewModel.isLoading {
+                ProgressView()
+            } else {
+                CardSwiperView(items: $viewModel.animals) { animal in
+                    AnimalCardView(animal: animal)
+                }
+            }
+        }
+        .onAppear {
+            viewModel.loadAnimals()
+        }
     }
 }
